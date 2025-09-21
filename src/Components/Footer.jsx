37e68feb7af0d98a4f2d47bd444ea2e17541207d
CManchi-1211 <ar113001@fhss.sjp.ac.lk>
2025-09-21 23:22:33 +0530

    Complete Footer.jsx

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default Footer = () => {
+  return (
+    <footer className="p-4 text-center text-gray-600 dark:text-gray-400">
+      <p>&copy; {new Date().getFullYear()} Chaya Manchanayake. All rights reserved.</p>
+    </footer>
+  );
+};
