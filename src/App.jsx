eef82b87684b06fb5ef51f7fd87a385a787ca7a5
CManchi-1211 <ar113001@fhss.sjp.ac.lk>
2025-09-24 23:40:02 +0530

    Update package.json and App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import  { ThemeProvider }  from './context/themeContext.jsx';
 import Home from './Pages/Home.jsx';
 import About from './Pages/About.jsx';
